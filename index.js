71be1b20d09fbf048896d9dadd9b30a2c7c0f8ce
Bennett <bjupfield@gmail.com>
2021-11-01 16:03:29 -0500

    c

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 31;
+let num2 = 2; 
+let num3 = 24;
+let num4 = 5;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4;
+const max = 20;
 // Write your code here
